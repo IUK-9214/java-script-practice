2b5a70735270a898bb731d6300026f44e9cc1b22
IBAD ULLAH KHAN <ibad50008@gmail.com>
2024-12-26 09:08:04 +0500

    start up

--- practice 01/file.js
+++ practice 01/file.js
@@ -1,18 +1,1 @@
-function mySuperpower(){
-    document.getElementById("demo2").innerHTML="hello i did it";
-    document.getElementById("demo2").style.fontSize="40px";
-   
-   
-}
-function mysuperpowers(){
-    document.getElementById("demo3").innerHTML=44+33;
-}
-document.write(3+3)
-window.alert("hello  i am a jarvis")
-function hit(){
-    window.print();
-}
-function myvar(){
-    let name="ibad ullah khan";
-    document.getElementById("demo4").innerHTML=name;
-}
+
