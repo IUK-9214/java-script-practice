20cd5f0b48ec14a408f3071b79369b38c37752e1
IBAD ULLAH KHAN <ibad50008@gmail.com>
2024-12-26 09:40:51 +0500

    alert function
    using the alret function

--- practice 0/file.js
+++ practice 0/file.js
@@ -1,3 +1,5 @@
+window.alert("this is dangerous for mind keep your self away from it ")
+
 function myfunction() {
   document.getElementById("demo").innerHTML =
     "this is the new era so learn quickly and alot of things ";
