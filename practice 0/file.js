09f5ff2309fb95ae0698123a4821b3e2fe8ca592
IBAD ULLAH KHAN <ibad50008@gmail.com>
2024-12-26 09:36:45 +0500

    progressing
    using the function in the web

--- practice 0/file.js
+++ practice 0/file.js
@@ -2,3 +2,6 @@ function myfunction() {
   document.getElementById("demo").innerHTML =
     "this is the new era so learn quickly and alot of things ";
 }
+function myanother(){
+document.write(5+4);
+}
